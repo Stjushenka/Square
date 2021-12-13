980e5174e8a2f75325cdcd530d7bb41c81fdf16a
Stjushenka <stjushenka@gmail.com>
2021-12-13 17:36:25 +0300

    SQRService

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,7 +4,7 @@ public class SQRService{
     public int calculate (int lower, int hight, int i) {
         int count = 0;
 
-        for (i = 0; i < 99; i++) {
+        for (i = 0; i <= 99; i++) {
             if (i * i > lower && i * i < hight) {
                 count++;
                 System.out.println("Квадрат " + i);
