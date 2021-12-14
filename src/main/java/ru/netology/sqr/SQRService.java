b0b322263cce221032f61ad20941c88cd4c915c9
Stjushenka <stjushenka@gmail.com>
2021-12-14 13:13:26 +0300

    Calculate

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,21 +1,19 @@
 package ru.netology.sqr;
 
-public class SQRService{
-    public int calculate (int lower, int hight) {
+public class SQRService {
+    public int calculate(int lower, int hight) {
         int count = 0;
 
         for (int i = 10; i <= 99; i++) {
             if (i * i >= lower & i * i <= hight) {
                 count++;
                 System.out.println("Квадрат " + i);
-                                }
+            }
         }
         System.out.println("Количество подходящих квадратов чисел - " + count);
-            return count;
+        return count;
     }
 
 
-
-
-    }
+}
 
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,7 +1,6 @@
 package ru.netology.sqr;
 
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -9,57 +8,57 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.CsvSource;
 
 
-    class SQRServiceTest {
-        @Test
-        public void testCalculate() {
-            SQRService service = new SQRService();
-            int number = service.calculate(200, 300);
-            assertEquals(3, number);
-
-        }
-
-        @Test
-        public void testCalculateLower() {
-            SQRService service = new SQRService();
-            int number = service.calculate(99, 300);
-            assertEquals(8, number);
-
-        }
-
-        @Test
-        public void testCalculateSqr() {
-            SQRService service = new SQRService();
-            int number = service.calculate(150, 300);
-            assertEquals(5, number);
-
-        }
-
-        @Test
-        public void testCalculateSqr2() {
-            SQRService service = new SQRService();
-
-            int number = service.calculate(0, 300);
-            assertEquals(8, number);
-
-        }
-
-        @Test
-        public void testCalculateSqr3() {
-            SQRService service = new SQRService();
-
-            int number = service.calculate(200, 600);
-            assertEquals(10, number);
-
-        }
-
-        @Test
-        public void testCalculateSqr0() {
-            SQRService service = new SQRService();
-
-            int number = service.calculate(260, 285);
-            assertEquals(0, number);
-
-        }
-
+class SQRServiceTest {
+    @Test
+    public void testCalculate() {
+        SQRService service = new SQRService();
+        int number = service.calculate(200, 300);
+        assertEquals(3, number);
 
     }
+
+    @Test
+    public void testCalculateLower() {
+        SQRService service = new SQRService();
+        int number = service.calculate(99, 300);
+        assertEquals(8, number);
+
+    }
+
+    @Test
+    public void testCalculateSqr() {
+        SQRService service = new SQRService();
+        int number = service.calculate(150, 300);
+        assertEquals(5, number);
+
+    }
+
+    @Test
+    public void testCalculateSqr2() {
+        SQRService service = new SQRService();
+
+        int number = service.calculate(0, 300);
+        assertEquals(8, number);
+
+    }
+
+    @Test
+    public void testCalculateSqr3() {
+        SQRService service = new SQRService();
+
+        int number = service.calculate(200, 600);
+        assertEquals(10, number);
+
+    }
+
+    @Test
+    public void testCalculateSqr0() {
+        SQRService service = new SQRService();
+
+        int number = service.calculate(260, 285);
+        assertEquals(0, number);
+
+    }
+
+
+}
